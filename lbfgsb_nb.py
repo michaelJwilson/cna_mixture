188438f4907cdfb1a673e2d6d9a5b5a8b0c441bb
Michael J. Wilson <michaeljwilson@princeton.edu>
2025-02-27 16:02:14 -0500

    fix

--- lbfgsb_nb.py
+++ lbfgsb_nb.py
@@ -1,10 +1,11 @@
-import numpy as np
 import torch
+import numpy as np
+import pylab as pl
 import torch.nn.functional as F
 
 from scipy.stats import nbinom, betabinom
 from scipy.special import digamma
-from scipy.optimize import approx_fprime, check_grad
+from scipy.optimize import approx_fprime, check_grad, minimize
 from scipy.differentiate import derivative
 
 
@@ -50,15 +51,17 @@ def grad_ln_nb_mu(k, mu, var):
     p = mu / var
     r = mu * mu / (var - mu)
 
-    return (2. * var - mu) * mu * grad_ln_nb_r(k, r, p) / (var - mu)**2. + (1. / var) * grad_ln_nb_p(k, r, p)
+    return (2.0 * var - mu) * mu * grad_ln_nb_r(k, r, p) / (var - mu) ** 2.0 + (
+        1.0 / var
+    ) * grad_ln_nb_p(k, r, p)
 
 
 def grad_ln_nb_var(k, mu, var):
     p = mu / var
     r = mu * mu / (var - mu)
 
-    result = -mu **2. * grad_ln_nb_r(k, r, p) / (var - mu)**2.
-    result -= (mu / var**2.) * grad_ln_nb_p(k, r, p)
+    result = -(mu**2.0) * grad_ln_nb_r(k, r, p) / (var - mu) ** 2.0
+    result -= (mu / var**2.0) * grad_ln_nb_p(k, r, p)
 
     return result
 
@@ -68,6 +71,24 @@ def grad_ln_nb_muvar(x, k):
     return np.array([grad_ln_nb_mu(k, mu, var), grad_ln_nb_var(k, mu, var)])
 
 
+def nloglikes(r, p, samples):
+    return np.array([-nbinom.logpmf(k, r, p) for k in samples])
+
+
+def nloglike(x, samples):
+    mu, var = x    
+    p = mu / var
+    r = mu * mu / (var - mu)
+
+    return nloglikes(r, p, samples).sum()
+
+def grad_nloglike(x, samples):
+    result = np.zeros(2)
+
+    for k in samples:
+        result += grad_ln_nb_muvar(x, k)
+
+    return -result
 
 if __name__ == "__main__":
     # NB https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.check_grad.html
@@ -92,11 +113,53 @@ if __name__ == "__main__":
     approx_grad = approx_fprime(x0, ln_nb_muvar, np.sqrt(np.finfo(float).eps), k)
 
     err = check_grad(ln_nb_muvar, grad_ln_nb_muvar, x0, k)
-    
+
     assert res == exp
     assert err < 2.0e-6, f""
 
+    samples = nbinom.rvs(r, p, size=10_000)
+    probs = nloglikes(r, p, samples)
 
+    # pl.plot(samples, probs, lw=0.0, marker='.')
+    # pl.show()
+    
+    # NB (mu, var) = (36.0 144.0)
+    x0 = np.array([50., 100.])
+    grad = grad_nloglike(x0, samples)
+
+    approx_grad = approx_fprime(x0, nloglike, np.sqrt(np.finfo(float).eps), samples)
+
+    err = check_grad(nloglike, grad_nloglike, x0, samples)
+    
+    print(grad)
+    print(approx_grad)
+
+    # assert err < 2.0e-6, f""
+    
+    epsilon = np.sqrt(np.finfo(float).eps)
+    bounds = [(epsilon, None), (epsilon, None)]
+    
+    # NB L-BFGS-B accepts bounds.
+    res = minimize(
+        nloglike,
+        x0,
+        args=(samples),
+        method="L-BFGS-B",
+        jac=grad_nloglike,
+        hess=None,
+        hessp=None,
+        bounds=bounds,
+        constraints=(),
+        tol=None,
+        callback=None,
+        options=None,
+    )
+
+    print(res)
+
+    
+
+    
     """
     k = torch.tensor(k, requires_grad=False)  # Number of successes
     mean = torch.tensor(mu, requires_grad=True)  # Mean of the distribution
