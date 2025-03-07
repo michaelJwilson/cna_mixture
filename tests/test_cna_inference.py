c98f2a0923043221fe6483a68b5fcc45ab652e40
Michael J. Wilson <michaeljwilson@princeton.edu>
2025-03-07 11:54:08 -0500

    fix

--- tests/test_cna_inference.py
+++ tests/test_cna_inference.py
@@ -7,38 +7,6 @@ from scipy.optimize import approx_fprime
 
 np.random.seed(314)
 
-
-def test_cna_mixture_params_dict_update(mixture_params):
-    assert mixture_params.num_states == len(mixture_params.cna_states)
-    assert np.array_equal(mixture_params.cna_states[0], np.array([1.0, 0.5]))
-
-    mixture_params.dict_update(get_sim_params())
-
-    exp = np.array([[1.0, 0.5], [3.0, 0.33], [4.0, 0.25], [10.0, 0.1]])
-
-    assert mixture_params.overdisp_tau == 45.0
-    assert mixture_params.overdisp_phi == 0.01
-    assert np.allclose(
-        mixture_params.cna_states,
-        exp,
-        atol=1e-1,
-    )
-
-
-def test_cna_mixture_params_rdr_baf_choice_update(mixture_params, rdr_baf):    
-    mixture_params.rdr_baf_choice_update(rdr_baf)
-
-    # TODO exp changes whether the test is run individually, or all tests run.
-    # NB matches rdr_baf realization
-    exp = [5.22208937, 8.13003172, 9.69945894]
-
-    assert np.allclose(
-        mixture_params.cna_states[1:, 0],
-        exp,
-        atol=1e-2,
-    )
-
-
 def test_em_cost_grad():
     cna_sim = CNA_sim()
     cna_model = CNA_inference(cna_sim.realized_genome_coverage, cna_sim.data)
