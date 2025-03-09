6f1334db8ca41279d7f23ee68f143f901d7ebb02
Michael J. Wilson <michaeljwilson@princeton.edu>
2025-03-09 11:25:45 -0400

    fix

--- python/cna_mixture/state_priors.py
+++ python/cna_mixture/state_priors.py
@@ -60,23 +60,41 @@ class CNA_categorical_prior:
 class CNA_markov_prior:
     def __init__(self, num_segments, jump_rate, num_states, ln_start_prior=None):
         if ln_start_prior is None:
-            ln_start_prior = np.log((1. / num_states) * np.ones(num_states))
+            ln_start_prior = np.log((1.0 / num_states) * np.ones(num_states))
 
         self.num_segments = num_segments
         self.ln_start_prior = ln_start_prior
-        self.transfer = CNA_transfer(jump_rate=jump_rate, num_states=num_states).transfer_matrix
+        self.transfer = CNA_transfer(
+            jump_rate=jump_rate, num_states=num_states
+        ).transfer_matrix
 
         self.ln_fs = np.zeros(shape=(num_segments, num_states))
-        
+        self.ln_bs = np.zeros(shape=(num_segments, num_states))
+
+    @staticmethod
+    def logTexp(transfer, ln_probs):
+        max_ln_probs = np.max(ln_probs, keepdims=True)
+        return max_ln_probs + np.log(np.dot(transfer, np.exp(ln_probs - max_ln_probs)))
+
     def forward(self, ln_state_emission):
-        self.ln_fs[0,:] = self.ln_start_prior + ln_state_emission[0,:]
+        self.ln_fs[0, :] = self.ln_start_prior + ln_state_emission[0, :]
 
         for ii in range(1, self.num_segments):
-            self.ln_fs[ii,:] = np.log(np.dot(self.transfer, np.exp(self.ln_fs[ii-1,:]).T))
-            self.ln_fs[ii,:] += ln_state_emission[ii,:]
-            
+            self.ln_fs[ii, :] = ln_state_emission[ii, :]
+            self.ln_fs[ii, :] += self.logTexp(self.transfer, self.ln_fs[ii - 1, :].T)
+
     def backward(self, ln_state_emission):
-        raise NotImplementedError()
-        
-    def get_state_priors(self, ln_state_emission):
-        raise NotImplementedError()
+        self.ln_bs[-1, :] = self.ln_start_prior
+
+        for ii in range(self.num_segments - 2, -1, -1):
+            self.ln_bs[ii, :] = self.logTexp(
+                self.transfer.T, self.ln_bs[ii + 1, :] + ln_state_emission[ii + 1, :]
+            )
+
+    def update(self, ln_state_emission):
+        self.forward(ln_state_emission)
+        self.backward(ln_state_emission)
+
+    def get_state_priors(self):
+        norm = logsumexp(self.ln_fs + self.ln_bs, axis=1)        
+        return -norm[:,None] + (self.ln_fs + self.ln_bs)
--- tests/test_state_priors.py
+++ tests/test_state_priors.py
@@ -12,8 +12,8 @@ np.random.seed(314)
 def test_CNA_categorical_prior(mixture_params, rdr_baf):
     equal_priors = CNA_categorical_prior.ln_lambdas_equal(5)
     state_priors = CNA_categorical_prior(mixture_params, rdr_baf)
-    
-    assert logsumexp(equal_priors) == 0.
+
+    assert logsumexp(equal_priors) == 0.0
     assert len(state_priors.ln_lambdas) == len(mixture_params.cna_states)
     assert np.abs(logsumexp(state_priors.ln_lambdas)) < 1.5e-16
 
@@ -23,22 +23,53 @@ def test_CNA_categorical_prior(mixture_params, rdr_baf):
     ln_state_posteriors = np.log(np.array([[0.25, 0.25, 0.1, 0.4]]))
 
     state_priors.update(ln_state_posteriors)
-    
-    npt.assert_allclose(ln_state_posteriors[0], state_priors.ln_lambdas, rtol=1e-5, atol=1e-8)
+
+    npt.assert_allclose(
+        ln_state_posteriors[0], state_priors.ln_lambdas, rtol=1e-5, atol=1e-8
+    )
 
     assert np.abs(logsumexp(state_priors.ln_lambdas)) < 1.5e-16
-    
+
     state_priors = state_priors.get_state_priors(5)
 
-    npt.assert_allclose(np.tile(ln_state_posteriors, (5, 1)), state_priors, rtol=1e-5, atol=1e-8)
+    npt.assert_allclose(
+        np.tile(ln_state_posteriors, (5, 1)), state_priors, rtol=1e-5, atol=1e-8
+    )
 
 
 def test_CNA_markov_prior():
-    markov_prior = CNA_markov_prior(num_segments=10, jump_rate=0.1, num_states=4, ln_start_prior=None)
+    num_segments, num_states, jump_rate = 10, 4, 0.2
 
-    xs = norm.rvs(loc=0., scale=1., size=10, random_state=None)
-    ln_state_emission = np.tile(norm.logpdf(xs, loc=0., scale=1.), (4, 1)).T
+    markov_prior = CNA_markov_prior(
+        num_segments=num_segments,
+        jump_rate=jump_rate,
+        num_states=num_states,
+        ln_start_prior=np.zeros(num_states)
+    )
+
+    states = np.array([np.random.randint(0, num_states) for ii in range(num_segments)])
+    samples = np.array([norm.rvs(loc=10 * ss, scale=10.0, size=1) for ss in states])
     
-    markov_prior.forward(ln_state_emission)
+    ln_state_emission = np.hstack([-0.5 * (samples - 10. * ii)**2. for ii in range(num_states)])
+    
+    assert ln_state_emission.shape == (num_segments, num_states)
 
-    print("\n", markov_prior.ln_fs)
+    print("\n", ln_state_emission)
+    
+    markov_prior.update(ln_state_emission)
+    
+    state_priors = markov_prior.get_state_priors()
+
+    print("\n", state_priors)
+
+    decoded_states = np.argmax(ln_state_emission, axis=1)
+    markov_decoded_states = np.argmax(state_priors, axis=1)
+    
+    print("\n\n")
+    
+    print(states)
+    print(decoded_states)
+    print(markov_decoded_states)
+    
+
+    
