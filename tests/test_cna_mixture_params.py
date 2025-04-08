d46adf6730441307dfe779760678543d798a7529
Michael J. Wilson <michaeljwilson@princeton.edu>
2025-04-08 10:44:18 -0400

    fix

--- tests/test_cna_mixture_params.py
+++ tests/test_cna_mixture_params.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
 
+import pytest
 import numpy as np
 from cna_mixture.cna_sim import get_sim_params
-from cna_mixture_params import CNA_mixture_params
+from cna_mixture.cna_mixture_params import CNA_mixture_params
 
 
 def test_cna_mixture_params_dict_update(mixture_params):
@@ -11,7 +12,13 @@ def test_cna_mixture_params_dict_update(mixture_params):
 
     # TODO HACK?
     mixture_params.dict_update(
-        get_sim_params() | {"genome_coverage": 500} | {"num_cna_states": 3}
+        get_sim_params()
+        | {
+            "genome_coverage": 500,
+            "num_cna_states": 3,
+            "seed": 42,
+            "rng": np.random.default_rng(42),
+        }
     )
 
     exp = np.array([[1.0, 0.5], [3.0, 0.33], [4.0, 0.25], [10.0, 0.1]])
@@ -41,9 +48,14 @@ def test_initialize_random_nonnormal_rdr_baf(mixture_params, rdr_baf):
 
 def test_cna_mixture_params_reproducibility(mixture_params):
     new_params = CNA_mixture_params(seed=314)
+    new_params.initialize()
 
-    print(mixture_params.params)
-    print(new_params.params)
+    assert np.all(mixture_params.params == new_params.params)
 
 
-# def test_cna_mixture_params_seeding(mixture_params):
+def test_cna_mixture_params_seeding(mixture_params):
+    new_params = CNA_mixture_params(seed=42)
+    new_params.initialize()
+
+    with pytest.raises(AssertionError):
+        assert np.all(mixture_params.params == new_params.params)
