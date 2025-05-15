819e1bd908a395f386b6de145b4b18beb0d8fa20
Michael J. Wilson <michaeljwilson@princeton.edu>
2025-05-15 11:29:36 -0400

    python shell

--- cna-mixture-rs/src/lib.rs
+++ cna-mixture-rs/src/lib.rs
@@ -12,8 +12,8 @@ use rayon::{ThreadPool, ThreadPoolBuilder};
 use statrs::function::gamma::{digamma, ln_gamma};
 use std::env;
 
-#[pyclass]
-struct CnaEmissionRs {
+
+pub struct CnaEmission {
     //  NB defining a struct associated locally in memory.
     ks: Vec<f64>,
     xs: Vec<f64>,
@@ -22,6 +22,80 @@ struct CnaEmissionRs {
     ws: Vec<f64>,
     mapping: Option(Vec<f64>),
     thread_pool: ThreadPool,
+}
+
+impl CnaEmissionRs {
+    #[new]
+    fn new(
+        ks: Vec<f64>,
+        xs: Vec<f64>,
+        bs: Vec<f64>,
+        ns: Vec<f64>,
+    ) -> Self {
+        let num_threads = env::var("RAYON_NUM_THREADS")
+            .ok()
+            .and_then(|v| v.parse().ok())
+            .unwrap_or_else(|| num_cpus::get());
+
+        let thread_pool = ThreadPoolBuilder::new()
+            .num_threads(num_threads)
+            .build()
+            .map_err(|e| pyo3::exceptions::PyRuntimeError::new_err(e.to_string()))?;
+
+        let mut unique_map: HashMap<(f64, f64), usize> = HashMap::new();
+
+        let mut unique_ks = Vec::new();
+        let mut unique_xs = Vec::new();
+
+        let mut mapping = Vec::new();
+
+        for (&k, &x) in izip!(ks.as_array().iter(), xs.as_array().iter()) {
+            let key = (k, x);
+
+            if let Some(&index) = unique_map.get(&key) {
+                mapping.push(index);
+            } else {
+                let new_index = unique_ks.len();
+
+                unique_map.insert(key, new_index);
+
+                unique_ks.push(k);
+                unique_xs.push(x);
+
+                mapping.push(new_index);
+            }
+        }
+
+        //  TODO to_vec() directly?
+        Ok(CnaEmission {
+            ks: unique_ks,
+            xs: unique_xs,
+            bs: bs.as_array().to_vec(),
+            ns: ns.as_array().to_vec(),
+            mapping: mapping,
+            thread_pool: thread_pool,
+        })
+    }
+
+    fn nbinom_logpmf_reduce(&self, _means: &[f64], overdisp: f64) -> f64 {
+        self.thread_pool
+            .install(|| nbinom_logpmf_reduce(&self.ks, &self.xs, means, overdisp))
+    }
+
+    fn betabinom_logpmf_reduce(
+        &self,
+        alphas: &[f64],
+        betas: &[f64],
+    ) -> f64 {
+        self.thread_pool
+            .install(|| betabinom_logpmf_reduce(&self.bs, &self.ns, alphas, betas))
+    }
+}
+
+
+#[pyclass]
+struct CnaEmissionRs {
+    inner: CnaEmission,
 }
 
 #[pymethods]
