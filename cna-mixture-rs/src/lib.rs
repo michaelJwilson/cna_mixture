c7fe4b0c605df1280f95eb3e2598fdae10a55f9d
Michael J. Wilson <michaeljwilson@princeton.edu>
2025-05-15 10:12:51 -0400

    fix

--- cna-mixture-rs/src/lib.rs
+++ cna-mixture-rs/src/lib.rs
@@ -150,7 +150,7 @@ fn nbinom_logpmf_rs<'py>(
     Ok(result)
 }
 
-// NB 4.4646 ms -> 22.998 µs
+// NB 288.46 µs
 pub fn betabinom_logpmf_reduce<'py>(k: &[f64], n: &[f64], a: &[f64], b: &[f64]) -> f64 {
     //
     //  Efficient beta binomial evaluation for many samples x many states.
@@ -233,7 +233,7 @@ fn betabinom_logpmf_rs<'py>(
     n: PyReadonlyArray1<'_, f64>,
     a: PyReadonlyArray1<'_, f64>,
     b: PyReadonlyArray1<'_, f64>,
-) -> PyResult<f64> {
+) -> PyResult<Vec<Vec<f64>>> {
     //
     //  Efficient beta binomial evaluation for many samples x many states.
     //
@@ -244,7 +244,7 @@ fn betabinom_logpmf_rs<'py>(
     let a = a.to_vec()?;
     let b = b.to_vec()?;
 
-    let result = betabinom_logpmf_reduce(&k, &n, &a, &b);
+    let result = betabinom_logpmf(&k, &n, &a, &b);
 
     Ok(result)
 }
