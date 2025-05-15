d36544bc544f9b689869ca55180d85c2213cbfe3
Michael J. Wilson <michaeljwilson@princeton.edu>
2025-05-15 14:37:28 -0400

    fix

--- tests/test_cna_emission.py
+++ tests/test_cna_emission.py
@@ -14,16 +14,18 @@ from scipy.stats import betabinom, nbinom
 
 np.random.seed(314)
 
+
 @pytest.fixture()
 def emission_params():
     # TODO
     num_states = 1
 
-    # NB nbinom.rvs(lost_reads, dropout_rate, size=1)[0]                                                                                                           
-    rdrs, phi = 1. + np.arange(num_states), 1.0e-2
-    bafs, tau = 0.2 * (1. + np.arange(num_states)), 10.0
+    # NB nbinom.rvs(lost_reads, dropout_rate, size=1)[0]
+    rdrs, phi = 1.0 + np.arange(num_states), 1.0e-2
+    bafs, tau = 0.2 * (1.0 + np.arange(num_states)), 10.0
 
     return rdrs, phi, bafs, tau
+
 
 @pytest.fixture()
 def emission(emission_params):
@@ -34,83 +36,68 @@ def emission(emission_params):
 
     # NB required for scipy sampling calls.
     assert num_states == 1
-    
+
     # NB segment reads covering a snp < all segment reads.
     assert snp_coverage < normal_coverage
     assert len(rdrs) == num_states
 
     # NB rr == 1/phi, i.e. equivalent for all states.
-    rr, pp = reparameterize_nbinom(normal_coverage * rdrs, phi)    
+    rr, pp = reparameterize_nbinom(normal_coverage * rdrs, phi)
     alphas, betas = reparameterize_beta_binom(bafs, tau)
-    
+
     # NB (fairly) assumes each spot and each segment has the same normal coverage.
     ks = nbinom.rvs(rr, pp, size=10_000).astype(np.float64)
     xs = normal_coverage * np.ones_like(ks)
 
-    bs = betabinom.rvs(                                                                                                                                            
-        snp_coverage, betas, alphas, size=10_000                                                                                        
-    ).astype(np.float64)
-    
-    ns = snp_coverage * np.ones_like(bs).astype(np.float64)                                                                                                            
+    bs = betabinom.rvs(snp_coverage, betas, alphas, size=10_000).astype(np.float64)
+
+    ns = snp_coverage * np.ones_like(bs).astype(np.float64)
     return CNA_emission(num_states, ks, xs, bs, ns)
-    
+
+
 def test_emission_fixture(emission, emission_params):
     assert emission is not None
     assert emission_params is not None
 
+
 # ----  rust backend tests  ----
 def test_nbinom_rs(benchmark, emission, emission_params):
     rdrs, phi, _, _ = emission_params
-    
+
     benchmark(lambda: nbinom_rs(emission.ks, emission.xs, rdrs, phi))
+
 
 def test_betabinom_rs(benchmark, emission, emission_params):
     _, _, bafs, tau = emission_params
     alphas, betas = reparameterize_beta_binom(bafs, tau)
-    
+
     benchmark(lambda: betabinom_rs(emission.bs, emission.ns, betas, alphas))
+
 
 @pytest.mark.parametrize("compress", [True, False])
 def test_cna_emission_rs_nb(benchmark, emission, emission_params, compress):
     rdrs, phi, _, _ = emission_params
-    
-    cna_em = CnaEmissionRs(emission.ks, emission.xs, emission.bs, emission.ns, compress=compress)
+
+    cna_em = CnaEmissionRs(
+        emission.ks, emission.xs, emission.bs, emission.ns, compress=compress
+    )
 
     result = benchmark(lambda: cna_em.nbinom_reduce(rdrs, phi))
+
 
 @pytest.mark.parametrize("compress", [True, False])
 def test_cna_emission_rs_bb(benchmark, emission, emission_params, compress):
     _, _, bafs, tau = emission_params
     alphas, betas = reparameterize_beta_binom(bafs, tau)
-    
-    cna_em = CnaEmissionRs(emission.ks, emission.xs, emission.bs, emission.ns, compress=compress)
+
+    cna_em = CnaEmissionRs(
+        emission.ks, emission.xs, emission.bs, emission.ns, compress=compress
+    )
 
     # TODO accept bafs, dispersion
     benchmark(lambda: cna_em.betabinom_reduce(alphas, betas))
-    
-"""
-# NB test rust backend.
-def test_cna_emission_rs_class(benchmark):
-    ks = 10. * np.ones(1_000)
-    xs = 2. * ks
 
-    bs = np.arange(1_000, dtype=float)
-    ns = np.arange(1_000, dtype=float)
 
-    cna_em = CnaEmissionRs(ks, xs, bs, ns, compress=False)
-    means = np.arange(10, dtype=float)
-    
-    result = benchmark(lambda: cna_em.nbinom_reduce(means, 1.e-2))
-
-def test_cna_emission_rs(benchmark):
-    ks = 10. * np.ones(1_000)
-    xs = 2. * ks
-
-    means = np.arange(10, dtype=float)
-
-    result = benchmark(lambda: nbinom_rs(ks, xs, means, 1.e-2))
-"""
-"""
 def test_cna_emission():
     num_states, normal_coverage, snp_coverage = 1, 10, 100
 
@@ -126,7 +113,7 @@ def test_cna_emission():
     xs = betabinom.rvs(
         snp_coverage, pseudo_counts[:, 1], pseudo_counts[:, 0], size=10_000
     ).astype(np.float64)
-    
+
     ns = snp_coverage * np.ones_like(xs).astype(np.float64)
 
     # NB RDR-like params are read depths, not RDR.
@@ -181,4 +168,3 @@ def test_cna_emission():
     grad = emission.grad_em_cost(params, state_posteriors)
 
     npt.assert_allclose(rust_grad, grad, rtol=1.0e-5, atol=1.0e-8)
-"""
