63633359f5dfa9c5b55a39992236ce3833f66bf8
Michael J. Wilson <michaeljwilson@princeton.edu>
2025-05-15 15:09:37 -0400

    fix

--- tests/test_cna_emission.py
+++ tests/test_cna_emission.py
@@ -99,32 +99,10 @@ def test_cna_emission_rs_bb(benchmark, emission, emission_params, compress):
     benchmark(lambda: cna_em.betabinom_reduce(alphas, betas))
 
 
-def test_cna_emission(emission, emission_params):
+def test_CNA_emission_bb(emission, emission_params):
     rdrs, phi, bafs, tau = emission_params
     params = np.array([*rdrs, phi, *bafs, tau])
     
-    """
-    num_states, normal_coverage, snp_coverage = 1, 10, 100
-
-    # NB nbinom.rvs(lost_reads, dropout_rate, size=1)[0]
-    means, phi = np.array(num_states * [100]), 1.0e-2
-
-    rr_pp = reparameterize_nbinom(normal_coverage * means, phi)
-    ks = nbinom.rvs(rr_pp[:, 0], rr_pp[:, 1], size=10_000).astype(np.float64)
-
-    bafs, tau = np.array(num_states * [0.2]), 10.0
-    pseudo_counts = reparameterize_beta_binom(bafs, tau)
-
-    xs = betabinom.rvs(
-        snp_coverage, pseudo_counts[:, 1], pseudo_counts[:, 0], size=10_000
-    ).astype(np.float64)
-
-    ns = snp_coverage * np.ones_like(xs).astype(np.float64)
-    
-    # NB RDR-like params are read depths, not RDR.
-    params = np.array([*(normal_coverage * means), phi, *bafs, tau])
-    emission = CNA_emission(num_states, normal_coverage, ks, xs, ns)
-    """
     unpacked = emission.unpack_params(emission_params)
     states_bag = emission.get_states_bag(emission_params)
 
@@ -146,6 +124,11 @@ def test_cna_emission(emission, emission_params):
     # NB all log probabilites should be <= 0
     assert np.all(bb_update <= 0.0)
 
+    
+def test_CNA_emission_nb(emission, emission_params):
+    rdrs, phi, bafs, tau = emission_params
+    params = np.array([*rdrs, phi, *bafs, tau])
+    
     # NB >>>>>>  nbinom checks.
     emission.RUST_BACKEND = True
 
@@ -160,14 +143,18 @@ def test_cna_emission(emission, emission_params):
     # NB all log probabilites should be <= 0
     assert np.all(nb_update <= 0.0)
 
+@pytest.mark.skip(reason="TODO rework gradient calc.")
+def test_CNA_emission_grad(emission, emission_params):
+    rdrs, phi, bafs, tau = emission_params
+    params = np.array([*rdrs, phi, *bafs, tau])
+    
     # NB >>>>>>  beta-binomial grad checks.
     state_posteriors = np.ones(shape=(10_000, 1))
-    """
+    
     emission.RUST_BACKEND = True
-    rust_grad = emission.grad_em_cost(params, state_posteriors)
+    rs_grad = emission.grad_em_cost(params, state_posteriors)
 
     emission.RUST_BACKEND = False
     grad = emission.grad_em_cost(params, state_posteriors)
 
     npt.assert_allclose(rust_grad, grad, rtol=1.0e-5, atol=1.0e-8)
-    """
