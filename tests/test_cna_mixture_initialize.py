f1bf863c8e1ce8ce98d2ea7e3d96e293de42a00d
Michael J. Wilson <michaeljwilson@princeton.edu>
2025-05-15 16:23:56 -0400

    fix

--- tests/test_cna_mixture_initialize.py
+++ tests/test_cna_mixture_initialize.py
@@ -3,12 +3,24 @@ from __future__ import annotations
 import numpy as np
 import pytest
 from cna_mixture.cna_mixture_params import CNA_mixture_params
+from cna_mixture.cna_mixture_initialize import CNA_mixture_initialize
 from cna_mixture.cna_sim import get_sim_params
 
 
-# exp = np.array([[1.0, 0.5], [3.0, 0.33], [4.0, 0.25], [10.0, 0.1]])
+def test_initialize_nonnormal(mixture_params):
+    initializer = CNA_mixture_initialize(mixture_params)    
+    initializer.random()
 
-def test_initialize_random_nonnormal_rdr_baf(mixture_params, rdr_baf):
+    exp = np.array([[1.0, 0.5], [3.0, 0.33], [4.0, 0.25], [10.0, 0.1]])
+
+    assert np.allclose(
+        initializer.cna_states[1:, 0],
+        exp,
+        atol=1e-2,
+    )
+
+"""
+def test_initialize_nonnormal(mixture_params, rdr_baf):
     mixture_params.initialize_random_nonnormal_rdr_baf(rdr_baf)
 
     # TODO exp changes whether the test is run individually, or all tests run.
@@ -35,3 +47,4 @@ def test_cna_mixture_params_seeding(mixture_params):
 
     with pytest.raises(AssertionError):
         assert np.all(mixture_params.params == new_params.params)
+"""
