cd8129c6a4d21323e4f38a3764b1059512c14086
Michael J. Wilson <michaeljwilson@princeton.edu>
2025-05-19 10:53:38 -0400

    fix

--- python/cna_mixture/scripts/run_inference.py
+++ python/cna_mixture/scripts/run_inference.py
@@ -46,7 +46,7 @@ def run_inference(
     )
     
     cna_inf.initialize(**kwargs)
-    """
+
     cna_inf.plot(
         plots_dir,
         cna_inf.initial_params,
@@ -57,7 +57,7 @@ def run_inference(
     res = cna_inf.fit()
 
     cna_inf.plot(plots_dir, res.x, "final", "Final state posteriors")
-    """
+
     logger.info(f"Done ({time.time() - start:.3f} seconds).\n\n")
 
 
