902b07c8a6810b756b98a77058ddc624610aede1
Michael J. Wilson <michaeljwilson@princeton.edu>
2025-05-19 13:59:38 -0400

    fix

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,6 @@
 import numpy as np
 import numpy.testing as npt
-from cna_mixture.utils import logmatexp
+from cna_mixture.utils import logmatexp, assign_closest
 
 
 def test_logmatexp():
@@ -11,3 +11,15 @@ def test_logmatexp():
     result = logmatexp(transfer, ln_probs)
 
     npt.assert_allclose(result, exp, rtol=1e-5, atol=1e-8)
+
+
+def test_assign_closest(rdr_baf):
+    centers = [[1.0, 0.5], [5.5, 0.25], [10.75, 0.3]]
+
+    # NB zero baseline_coverage
+    rdr_baf[10,0] = np.inf
+    
+    idx = assign_closest(rdr_baf, centers)
+
+    print(rdr_baf)
+    print(idx)
